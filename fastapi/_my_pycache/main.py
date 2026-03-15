5c666eeed20cdaf29a567a0886f3e506b813cdf3
Alejandro lanchimba vivas Jaguar_EW <jlanchimbav@unemi.edu.ec>
2026-03-15 18:02:58 -0500

    main

--- fastapi/_my_pycache/main.py
+++ fastapi/_my_pycache/main.py
@@ -1,11 +1,12 @@
 
+from fastapi import FastAPI 
+from fastapi  import routers
 
-from fastapi import FastAPI
-from routers import items
-
-APP.include_routers(items.routers) # type: ignore
+APP= FastAPI()
+APP.include_routers(routers)
 APP= FastAPI()
 
 @APP.get("/")
 def read_root():
     return {"Hello": "World"}
+
