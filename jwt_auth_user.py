7c5e1bd3ebb114be29e592153e8cb1250c5a5934
Alejandro lanchimba vivas Jaguar_EW <https://github.com/jlanchimbav-gif>
2026-03-25 17:22:34 -0500

    main

--- jwt_auth_user.py
+++ jwt_auth_user.py
@@ -1,12 +1,18 @@
+from http.client import HTTPException
+import token
+
+from fastapi._my_pycache.routers.users import search_user, users
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi import Depends
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
 
 
 ALGORITHMS=["HS256"]
 SECRET_KEY = "your-secret-key"
+Access_TOKEN_DURATION=1
 
 app= FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -49,6 +55,27 @@ def search_user_db(username: str):
         return UserDB(**user_db[username])
     return None
 
+async def auth_user(token=str=Depends(oauth2_scheme)):
+    try:
+        username = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHMS]).get("sub")
+        if username is None:
+            raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"})
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"})
+
+async def current_users(users:users=Depends(auth_user)):
+    user = search_user(users)
+    if user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user")
+    return user
 
 @app.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
@@ -57,8 +84,16 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
          raise HTTPException(
             status_code=400, detail="Incorrect username or password")
 user= search_user(form.username)
-if form.password != user.password:
+
+if not crypt.verify(form.password, user.password):
         raise HTTPException(
             status_code=400, detail="Incorrect username or password")
+
+
+access_token={"sub": user.username,
+              "exp": datetime.utcnow() + timedelta(minutes=Access_TOKEN_DURATION) }
         
-    return{"access_token": user.username, "token_type": "bearer"}
+return{"access_token": jwt.encode(access_token, SECRET_KEY, algorithm=ALGORITHMS[0]), "token_type": "bearer"}
+
+
+
